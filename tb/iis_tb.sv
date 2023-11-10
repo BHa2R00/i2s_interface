cf47bea5370a6dbe5a331db5c766de72395f2b94
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2023-11-10 20:52:09 +0800

    Update iis_tb.sv

--- tb/iis_tb.sv
+++ tb/iis_tb.sv
@@ -17,16 +17,16 @@ wire u0_iis_rx_data_l_empty, u0_iis_rx_data_r_empty;
 wire u0_iis_tx;
 wire [31:0] u0_iis_rx_data_l, u0_iis_rx_data_r;
 reg [31:0] u0_iis_tx_data_l, u0_iis_tx_data_r;
-reg u0_iis_tx_data_fill;
+reg u0_iis_tx_data_fill, u0_iis_tx_data_clear;
 reg u0_iis_rx;
-reg u0_iis_rx_data_drain;
+reg u0_iis_rx_data_drain, u0_iis_rx_data_clear;
 iis u0_iis(
 	.rx(u0_iis_rx), 
 	.rx_data_r_full(), .rx_data_r_empty(u0_iis_rx_data_r_empty), 
 	.rx_data_l_full(), .rx_data_l_empty(u0_iis_rx_data_l_empty), 
 	.rx_data_l_count(), .rx_data_r_count(), 
 	.rx_data_depth(3'd5), 
-	.rx_data_drain(u0_iis_rx_data_drain), .rx_data_clear(1'b0), 
+	.rx_data_drain(u0_iis_rx_data_drain), .rx_data_clear(u0_iis_rx_data_clear), 
 	.rx_data_l(u0_iis_rx_data_l), .rx_data_r(u0_iis_rx_data_r), 
 	.enable_rx(1'b1), .rx_bpol(1'b1), .rx_lrpol(1'b1), 	// rx_bpol=1 -- read bit at rising edge, lrpol=1 -- left channel start at rising edge
 	.tx(u0_iis_tx), 
@@ -34,7 +34,7 @@ iis u0_iis(
 	.tx_data_l_full(u0_iis_tx_data_l_full), .tx_data_l_empty(), 
 	.tx_data_l_count(), .tx_data_r_count(), 
 	.tx_data_depth(3'd5), 
-	.tx_data_fill(u0_iis_tx_data_fill), .tx_data_clear(1'b0), 
+	.tx_data_fill(u0_iis_tx_data_fill), .tx_data_clear(u0_iis_tx_data_clear), 
 	.tx_data_l(u0_iis_tx_data_l), .tx_data_r(u0_iis_tx_data_r), 
 	.enable_tx(1'b1), .tx_bpol(1'b0), .tx_lrpol(1'b1), 	// tx_bpol=1 -- send bit at rising edge, lrpol=1 -- left channel start at rising edge
 	.msb_delay_bits(4'd1), 			// msb delay bits, 0 -- left-justified, 1 -- standard 
@@ -51,16 +51,16 @@ wire u1_iis_rx_data_l_empty, u1_iis_rx_data_r_empty;
 wire u1_iis_tx;
 wire [31:0] u1_iis_rx_data_l, u1_iis_rx_data_r;
 reg [31:0] u1_iis_tx_data_l, u1_iis_tx_data_r;
-reg u1_iis_tx_data_fill;
+reg u1_iis_tx_data_fill, u1_iis_tx_data_clear;
 reg u1_iis_rx;
-reg u1_iis_rx_data_drain;
+reg u1_iis_rx_data_drain, u1_iis_rx_data_clear;
 iis u1_iis(
 	.rx(u1_iis_rx), 
 	.rx_data_r_full(), .rx_data_r_empty(u1_iis_rx_data_r_empty), 
 	.rx_data_l_full(), .rx_data_l_empty(u1_iis_rx_data_l_empty), 
 	.rx_data_l_count(), .rx_data_r_count(), 
 	.rx_data_depth(3'd5), 
-	.rx_data_drain(u1_iis_rx_data_drain), .rx_data_clear(1'b0), 
+	.rx_data_drain(u1_iis_rx_data_drain), .rx_data_clear(u1_iis_rx_data_clear), 
 	.rx_data_l(u1_iis_rx_data_l), .rx_data_r(u1_iis_rx_data_r), 
 	.enable_rx(1'b1), .rx_bpol(1'b1), .rx_lrpol(1'b1), 	// rx_bpol=1 -- read bit at rising edge, lrpol=1 -- left channel start at rising edge
 	.tx(u1_iis_tx), 
@@ -68,7 +68,7 @@ iis u1_iis(
 	.tx_data_l_full(u1_iis_tx_data_l_full), .tx_data_l_empty(), 
 	.tx_data_l_count(), .tx_data_r_count(), 
 	.tx_data_depth(3'd5), 
-	.tx_data_fill(u1_iis_tx_data_fill), .tx_data_clear(1'b0), 
+	.tx_data_fill(u1_iis_tx_data_fill), .tx_data_clear(u1_iis_tx_data_clear), 
 	.tx_data_l(u1_iis_tx_data_l), .tx_data_r(u1_iis_tx_data_r), 
 	.enable_tx(1'b1), .tx_bpol(1'b0), .tx_lrpol(1'b1), 	// tx_bpol=1 -- send bit at rising edge, lrpol=1 -- left channel start at rising edge
 	.msb_delay_bits(4'd1), 			// msb delay bits, 0 -- left-justified, 1 -- standard 
@@ -93,7 +93,7 @@ initial u1_iis_rx_data_drain = 1'b0;
 always@(posedge clk_112m) if(rstn) begin
 	if(u0_iis_tx_data_fill) u0_iis_tx_data_fill = 1'b0;
 	else begin
-		if(~u0_iis_tx_data_l_full && ~u0_iis_tx_data_r_full) begin
+		if(~u0_iis_tx_data_l_full && ~u0_iis_tx_data_r_full && ~u0_iis_tx_data_clear) begin
 			u0_iis_tx_data_fill = 1'b1;
 			u0_iis_tx_data_l = (u0_iis_tx_data_l == 32'b10110111011110111110111111011111) ? 32'b01001000100001000001000000100000 : 32'b10110111011110111110111111011111;
 			u0_iis_tx_data_r = (u0_iis_tx_data_r == 32'b11111011111101111101111011101101) ? 32'b00000100000010000010000100010010 : 32'b11111011111101111101111011101101;
@@ -101,7 +101,7 @@ always@(posedge clk_112m) if(rstn) begin
 	end
 	if(u1_iis_tx_data_fill) u1_iis_tx_data_fill = 1'b0;
 	else begin
-		if(~u1_iis_tx_data_l_full && ~u1_iis_tx_data_r_full) begin
+		if(~u1_iis_tx_data_l_full && ~u1_iis_tx_data_r_full && ~u1_iis_tx_data_clear) begin
 			u1_iis_tx_data_fill = 1'b1;
 			u1_iis_tx_data_l = (u1_iis_tx_data_l == 32'b11111011111101111101111011101101) ? 32'b00000100000010000010000100010010 : 32'b11111011111101111101111011101101;
 			u1_iis_tx_data_r = (u1_iis_tx_data_r == 32'b10110111011110111110111111011111) ? 32'b01001000100001000001000000100000 : 32'b10110111011110111110111111011111;
@@ -115,6 +115,17 @@ always@(posedge clk_112m) if(rstn) begin
 	else begin
 		if(~u1_iis_rx_data_l_empty && ~u1_iis_rx_data_r_empty) u1_iis_rx_data_drain = 1'b1;
 	end
+end
+initial begin
+	u0_iis_tx_data_clear = 1'b1;
+	u0_iis_rx_data_clear = 1'b1;
+	u1_iis_tx_data_clear = 1'b1;
+	u1_iis_rx_data_clear = 1'b1;
+	#35000;
+	u0_iis_tx_data_clear = 1'b0;
+	u0_iis_rx_data_clear = 1'b0;
+	u1_iis_tx_data_clear = 1'b0;
+	u1_iis_rx_data_clear = 1'b0;
 end
 
 initial begin
